119b4b0fb9ea1a5f9cb59a372818a9f81aa5fdf7
leabyss <lea_hennebert@hotmail.fr>
2017-07-21 22:47:08 +0200

    step 8, 9, 10 done

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var display = document.getElementById("display");
 /* var multiplier = document.getElementById("multiplier").setAttribute("multiplier", 1); */
 var multiplier = 1;
 var displayTwo = document.getElementById("display2");
+var cost = 50;
 
 /* Clicker function *
  * Increase user's score by clicking. */
@@ -18,11 +19,13 @@ function increaseScore() {
 /* Multiply function *
  * Increase multiplier if score is big enough. */
 function increaseMultiplier() {
-    if (score > 50) {
+    if (score >= cost) {
         multiplier = multiplier + 1;
-        document.getElementById("display2").innerHTML = multiplier;
-        score = score - 50;
+        document.getElementById("display2").innerHTML = 'x '+multiplier;
+        score = score - cost;
         document.getElementById("display").innerHTML = score;
+        cost = cost * 2;
+        document.getElementById("cost").innerHTML = 'Need '+cost+' to increase click.';
     }
     else {
         alert("Sorry dude, you didn't score enough to own this.");
