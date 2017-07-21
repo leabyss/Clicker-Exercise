aab6a3fe296317991b44a0513c4d5c183f0bcb96
leabyss <lea_hennebert@hotmail.fr>
2017-07-21 08:13:56 +0200

    style modif + try step 5

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+/* * * * * * * VIRER LES TRUCS QUI SERVENT A RIEN APRES + RANGER * * * * * * */
+
 // Definition of variables
 var score = 0;
 var clickerButton = document.getElementById("clicker");
 var display = document.getElementById("display");
-var multiplier = document.getElementById("multiply").setAttribute("multiply", 1);
+/* var multiplier = document.getElementById("multiplier").setAttribute("multiplier", 1); */
+var multiplier = 1;
+var displayTwo = document.getElementById("display2");
 
 /* Clicker function *
  * Increase user's score by 1. */
-function increaseScore(element) {
+function increaseScore() {
     score = score + 1;
     document.getElementById("display").innerHTML = score;
 }
@@ -14,9 +18,10 @@ function increaseScore(element) {
 /* Multiply function *
  * Increase multiplier. */
 function increaseMultiplier() {
+    multiplier = multiplier + 1;
+    document.getElementById("display2").innerHTML = multiplier;
+    score = multiplier;
     /* When "multiply" is clicked *
      * score increases according to the multiplier *
-     * example : multiplier x2 = 2 / x3 = 6 / x4 = 18 *
-     * it's not what i'm supposed to do *
-     * but i don't really get it, cause I suck at mathematics */
+     * example : x2 = 2 / x12 = 12 etc... */
 }
